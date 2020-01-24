337400bec3e2789028cf55e400b35a7a98151751
kadono <y.kadono4218@gmail.com>
2020-01-24 18:02:56 +0900

    delete unused import

--- src/main/java/practice/jwt/controller/TestController.java
+++ src/main/java/practice/jwt/controller/TestController.java
@@ -1,7 +1,6 @@
 package practice.jwt.controller;
 
 import org.springframework.http.*;
-import org.springframework.security.core.*;
 import org.springframework.security.core.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import practice.jwt.domain.entity.*;
